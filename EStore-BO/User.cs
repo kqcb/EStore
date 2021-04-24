6dc692f38ceb7207ed5c3e7e3c3ac8bb18bbde50
yllxgashi <yll@kutia.net>
2021-04-24 16:03:13 +0200

    create model classes

---
+++ EStore-BO/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EStore_BO
+{
+    class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
