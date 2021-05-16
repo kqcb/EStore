4a441122a88311da3f0508c28a7a3e78116db816
yllxgashi <yll@kutia.net>
2021-05-16 23:12:28 +0200

    add layers

---
+++ EStore-DAL/DatabaseConn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EStore_DAL
+{
+    class DatabaseConn
+    {
+        public static string conString = ConfigurationManager.ConnectionStrings["conStringEStore-Client"].ToString();
+        public static SqlConnection conn;
+        public static SqlCommand cmd;
+        public static SqlDataAdapter da;
+    }
+}
---
+++ EStore-DAL/ItemDAL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EStore_DAL
+{
+    public class ItemDAL
+    {
+        public DataTable GetAllItems()
+        {
+            try
+            {
+                using (DatabaseConn.conn = new SqlConnection(DatabaseConn.conString))
+                {
+                    DatabaseConn.da = new SqlDataAdapter("usp_GetAllItems", DatabaseConn.conn);
+                    DataTable dTable = new DataTable();
+                    DatabaseConn.da.Fill(dTable);
+
+                    return dTable;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
