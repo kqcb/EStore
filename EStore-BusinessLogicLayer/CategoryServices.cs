1ef3f885c6ce1d54af66987e2c60229091409b82
Blendon <blendonluta98@gmail.com>
2021-05-17 06:42:48 +0200

    changed names

--- EStore-BusinessLogicLayer/CategoryServices.cs
+++
@@ -1,9 +0,0 @@
-﻿using EStore_BusinessObjects;
-using EStore_DataAccessLayer.Mapper.Models;
-
-namespace EStore_BusinessLogicLayer
-{
-    public class CategoryServices : DbSet<Category>
-    {
-    }
-}
--- EStore-BusinessObjects/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EStore_BusinessObjects
-{
-    public class Category
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public Category ParentCategory { get; set; }
-    }
-}
