f21e42bb5f9884591f18d0b1f5b8ecd29b5eb21f
Blendon <blendonluta98@gmail.com>
2021-05-17 05:59:41 +0200

    add mapper/ fix internal classes

---
+++ EStore-DataAccessLayer/Context.cs
@@ -0,0 +1,21 @@
+﻿using EStore_BusinessObjects;
+using EStore_DataAccessLayer.Mapper.Models;
+
+namespace EStore_DataAccessLayer
+{
+    public class EStoreContext 
+    {
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+
+     
+
+        
+    }
+}
---
+++ EStore-DataAccessLayer/Mapper/Utils/Connection.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.SqlClient;
+
+namespace EStoreDataAccess.Utils
+{
+    public class Connection
+
+    {
+
+
+        public static SqlConnection GetSqlConnection(string name = "SEMSDefault")
+        {
+            return new SqlConnection(GetConnectionString(name));
+        }
+
+        public static string GetConnectionString(string name = "SEMSDefault")
+        {
+            //ConfigurationManager.ConnectionStrings[""].ConnectionString.ToString();
+            return "Data Source=LENOVO-Y700; Database=SEMS; Initial Catalog=SEMS; User ID=sa; Password=hejhej";
+        }
+
+
+        public static SqlCommand GetSqlCommand(SqlConnection conn, string procName)
+        {
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = procName;
+
+            return cmd;
+        }
+    }
+}
