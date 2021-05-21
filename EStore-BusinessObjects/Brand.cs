28ecaf3d2fb830fc7e32581c17909ff1bce631e5
Blendon <blendonluta98@gmail.com>
2021-05-22 00:17:41 +0200

    add folder

--- EStore-BusinessObjects/Brand.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EStore_BusinessObjects
-{
-    public class Brand
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        
-    }
-}
--- EStore-BusinessObjects/City.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EStore_BusinessObjects
-{
-    public class City
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
