ce699bfb53c21d3c9ab0400c8d7fe14edac034e3
Arbonita Sylejmani <arbonitasy@gmail.com>
2021-06-17 14:46:35 +0200

    telerik fix

---
+++ EStore_Backup_2021.06.16_07.31.46/Auth/SignUpControl.Designer.cs
@@ -0,0 +1,295 @@
+﻿
+namespace EStore.Auth
+{
+    partial class SignUpControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+               private void InitializeComponent()
+        {
+            this.htmlLabel1 = new MetroFramework.Drawing.Html.HtmlLabel();
+            this.htmlLabel6 = new MetroFramework.Drawing.Html.HtmlLabel();
+            this.htmlLabel7 = new MetroFramework.Drawing.Html.HtmlLabel();
+            this.htmlLabel8 = new MetroFramework.Drawing.Html.HtmlLabel();
+            this.htmlLabel2 = new MetroFramework.Drawing.Html.HtmlLabel();
+            this.txtName = new MetroFramework.Controls.MetroTextBox();
+            this.txtLastName = new MetroFramework.Controls.MetroTextBox();
+            this.txtEmail = new MetroFramework.Controls.MetroTextBox();
+            this.txtPassword = new MetroFramework.Controls.MetroTextBox();
+            this.cmbCities = new MetroFramework.Controls.MetroComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // htmlLabel1
+            // 
+            this.htmlLabel1.AutoScroll = true;
+            this.htmlLabel1.AutoScrollMinSize = new System.Drawing.Size(84, 41);
+            this.htmlLabel1.AutoSize = false;
+            this.htmlLabel1.BackColor = System.Drawing.SystemColors.Window;
+            this.htmlLabel1.Location = new System.Drawing.Point(8, 19);
+            this.htmlLabel1.Name = "htmlLabel1";
+            this.htmlLabel1.Size = new System.Drawing.Size(75, 23);
+            this.htmlLabel1.TabIndex = 19;
+            this.htmlLabel1.Text = "Name";
+            // 
+            // htmlLabel6
+            // 
+            this.htmlLabel6.AutoScroll = true;
+            this.htmlLabel6.AutoScrollMinSize = new System.Drawing.Size(140, 41);
+            this.htmlLabel6.AutoSize = false;
+            this.htmlLabel6.BackColor = System.Drawing.SystemColors.Window;
+            this.htmlLabel6.Location = new System.Drawing.Point(8, 81);
+            this.htmlLabel6.Name = "htmlLabel6";
+            this.htmlLabel6.Size = new System.Drawing.Size(75, 23);
+            this.htmlLabel6.TabIndex = 25;
+            this.htmlLabel6.Text = "Last name";
+            // 
+            // htmlLabel7
+            // 
+            this.htmlLabel7.AutoScroll = true;
+            this.htmlLabel7.AutoScrollMinSize = new System.Drawing.Size(79, 41);
+            this.htmlLabel7.AutoSize = false;
+            this.htmlLabel7.BackColor = System.Drawing.SystemColors.Window;
+            this.htmlLabel7.Location = new System.Drawing.Point(8, 143);
+            this.htmlLabel7.Name = "htmlLabel7";
+            this.htmlLabel7.Size = new System.Drawing.Size(75, 23);
+            this.htmlLabel7.TabIndex = 27;
+            this.htmlLabel7.Text = "Email";
+            // 
+            // htmlLabel8
+            // 
+            this.htmlLabel8.AutoScroll = true;
+            this.htmlLabel8.AutoScrollMinSize = new System.Drawing.Size(131, 41);
+            this.htmlLabel8.AutoSize = false;
+            this.htmlLabel8.BackColor = System.Drawing.SystemColors.Window;
+            this.htmlLabel8.Location = new System.Drawing.Point(8, 205);
+            this.htmlLabel8.Name = "htmlLabel8";
+            this.htmlLabel8.Size = new System.Drawing.Size(75, 23);
+            this.htmlLabel8.TabIndex = 29;
+            this.htmlLabel8.Text = "Password";
+            // 
+            // htmlLabel2
+            // 
+            this.htmlLabel2.AutoScroll = true;
+            this.htmlLabel2.AutoScrollMinSize = new System.Drawing.Size(59, 41);
+            this.htmlLabel2.AutoSize = false;
+            this.htmlLabel2.BackColor = System.Drawing.SystemColors.Window;
+            this.htmlLabel2.Location = new System.Drawing.Point(8, 267);
+            this.htmlLabel2.Name = "htmlLabel2";
+            this.htmlLabel2.Size = new System.Drawing.Size(75, 23);
+            this.htmlLabel2.TabIndex = 31;
+            this.htmlLabel2.Text = "City";
+            // 
+            // txtName
+            // 
+            // 
+            // 
+            // 
+            this.txtName.CustomButton.Image = null;
+            this.txtName.CustomButton.Location = new System.Drawing.Point(162, 1);
+            this.txtName.CustomButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtName.CustomButton.Name = "";
+            this.txtName.CustomButton.Size = new System.Drawing.Size(23, 23);
+            this.txtName.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtName.CustomButton.TabIndex = 1;
+            this.txtName.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtName.CustomButton.UseSelectable = true;
+            this.txtName.CustomButton.Visible = false;
+            this.txtName.Lines = new string[0];
+            this.txtName.Location = new System.Drawing.Point(8, 50);
+            this.txtName.MaxLength = 32767;
+            this.txtName.Name = "txtName";
+            this.txtName.PasswordChar = '\0';
+            this.txtName.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtName.SelectedText = "";
+            this.txtName.SelectionLength = 0;
+            this.txtName.SelectionStart = 0;
+            this.txtName.ShortcutsEnabled = true;
+            this.txtName.Size = new System.Drawing.Size(186, 25);
+            this.txtName.Style = MetroFramework.MetroColorStyle.Pink;
+            this.txtName.TabIndex = 33;
+            this.txtName.UseSelectable = true;
+            this.txtName.UseStyleColors = true;
+            this.txtName.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtName.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // txtLastName
+            // 
+            // 
+            // 
+            // 
+            this.txtLastName.CustomButton.Image = null;
+            this.txtLastName.CustomButton.Location = new System.Drawing.Point(162, 1);
+            this.txtLastName.CustomButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtLastName.CustomButton.Name = "";
+            this.txtLastName.CustomButton.Size = new System.Drawing.Size(23, 23);
+            this.txtLastName.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtLastName.CustomButton.TabIndex = 1;
+            this.txtLastName.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtLastName.CustomButton.UseSelectable = true;
+            this.txtLastName.CustomButton.Visible = false;
+            this.txtLastName.Lines = new string[0];
+            this.txtLastName.Location = new System.Drawing.Point(8, 112);
+            this.txtLastName.MaxLength = 32767;
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.PasswordChar = '\0';
+            this.txtLastName.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtLastName.SelectedText = "";
+            this.txtLastName.SelectionLength = 0;
+            this.txtLastName.SelectionStart = 0;
+            this.txtLastName.ShortcutsEnabled = true;
+            this.txtLastName.Size = new System.Drawing.Size(186, 25);
+            this.txtLastName.Style = MetroFramework.MetroColorStyle.Pink;
+            this.txtLastName.TabIndex = 34;
+            this.txtLastName.UseSelectable = true;
+            this.txtLastName.UseStyleColors = true;
+            this.txtLastName.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtLastName.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // txtEmail
+            // 
+            // 
+            // 
+            // 
+            this.txtEmail.CustomButton.Image = null;
+            this.txtEmail.CustomButton.Location = new System.Drawing.Point(162, 1);
+            this.txtEmail.CustomButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtEmail.CustomButton.Name = "";
+            this.txtEmail.CustomButton.Size = new System.Drawing.Size(23, 23);
+            this.txtEmail.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtEmail.CustomButton.TabIndex = 1;
+            this.txtEmail.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtEmail.CustomButton.UseSelectable = true;
+            this.txtEmail.CustomButton.Visible = false;
+            this.txtEmail.Lines = new string[0];
+            this.txtEmail.Location = new System.Drawing.Point(8, 172);
+            this.txtEmail.MaxLength = 32767;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.PasswordChar = '\0';
+            this.txtEmail.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtEmail.SelectedText = "";
+            this.txtEmail.SelectionLength = 0;
+            this.txtEmail.SelectionStart = 0;
+            this.txtEmail.ShortcutsEnabled = true;
+            this.txtEmail.Size = new System.Drawing.Size(186, 25);
+            this.txtEmail.Style = MetroFramework.MetroColorStyle.Pink;
+            this.txtEmail.TabIndex = 35;
+            this.txtEmail.UseSelectable = true;
+            this.txtEmail.UseStyleColors = true;
+            this.txtEmail.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtEmail.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // txtPassword
+            // 
+            // 
+            // 
+            // 
+            this.txtPassword.CustomButton.Image = null;
+            this.txtPassword.CustomButton.Location = new System.Drawing.Point(162, 1);
+            this.txtPassword.CustomButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtPassword.CustomButton.Name = "";
+            this.txtPassword.CustomButton.Size = new System.Drawing.Size(23, 23);
+            this.txtPassword.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtPassword.CustomButton.TabIndex = 1;
+            this.txtPassword.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtPassword.CustomButton.UseSelectable = true;
+            this.txtPassword.CustomButton.Visible = false;
+            this.txtPassword.Lines = new string[0];
+            this.txtPassword.Location = new System.Drawing.Point(8, 234);
+            this.txtPassword.MaxLength = 32767;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '●';
+            this.txtPassword.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtPassword.SelectedText = "";
+            this.txtPassword.SelectionLength = 0;
+            this.txtPassword.SelectionStart = 0;
+            this.txtPassword.ShortcutsEnabled = true;
+            this.txtPassword.Size = new System.Drawing.Size(186, 25);
+            this.txtPassword.Style = MetroFramework.MetroColorStyle.Pink;
+            this.txtPassword.TabIndex = 36;
+            this.txtPassword.UseSelectable = true;
+            this.txtPassword.UseStyleColors = true;
+            this.txtPassword.UseSystemPasswordChar = true;
+            this.txtPassword.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtPassword.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // cmbCities
+            // 
+            this.cmbCities.FormattingEnabled = true;
+            this.cmbCities.ItemHeight = 24;
+            this.cmbCities.Location = new System.Drawing.Point(8, 292);
+            this.cmbCities.Name = "cmbCities";
+            this.cmbCities.Size = new System.Drawing.Size(186, 30);
+            this.cmbCities.Style = MetroFramework.MetroColorStyle.Pink;
+            this.cmbCities.TabIndex = 40;
+            this.cmbCities.UseSelectable = true;
+            this.cmbCities.UseStyleColors = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.htmlLabel1);
+            this.panel1.Controls.Add(this.cmbCities);
+            this.panel1.Controls.Add(this.htmlLabel6);
+            this.panel1.Controls.Add(this.htmlLabel7);
+            this.panel1.Controls.Add(this.htmlLabel8);
+            this.panel1.Controls.Add(this.txtPassword);
+            this.panel1.Controls.Add(this.htmlLabel2);
+            this.panel1.Controls.Add(this.txtEmail);
+            this.panel1.Controls.Add(this.txtName);
+            this.panel1.Controls.Add(this.txtLastName);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(247, 375);
+            this.panel1.TabIndex = 41;
+            // 
+            // SignUpControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 36F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Bahnschrift", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "SignUpControl";
+            this.Size = new System.Drawing.Size(247, 375);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private MetroFramework.Drawing.Html.HtmlLabel htmlLabel1;
+        private MetroFramework.Drawing.Html.HtmlLabel htmlLabel6;
+        private MetroFramework.Drawing.Html.HtmlLabel htmlLabel7;
+        private MetroFramework.Drawing.Html.HtmlLabel htmlLabel8;
+        private MetroFramework.Drawing.Html.HtmlLabel htmlLabel2;
+        private MetroFramework.Controls.MetroTextBox txtName;
+        private MetroFramework.Controls.MetroTextBox txtLastName;
+        private MetroFramework.Controls.MetroTextBox txtEmail;
+        private MetroFramework.Controls.MetroTextBox txtPassword;
+        private MetroFramework.Controls.MetroComboBox cmbCities;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ EStore_Backup_2021.06.16_07.31.46/ClientsView/ClientsMainControl.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace EStore.ClientsView
+{
+    partial class ClientsMainControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgClients = new MetroFramework.Controls.MetroGrid();
+            this.tileCreate = new MetroFramework.Controls.MetroTile();
+            ((System.ComponentModel.ISupportInitialize)(this.dgClients)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgClients
+            // 
+            this.dgClients.AllowUserToAddRows = false;
+            this.dgClients.AllowUserToDeleteRows = false;
+            this.dgClients.AllowUserToResizeRows = false;
+            this.dgClients.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dgClients.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgClients.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dgClients.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgClients.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgClients.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgClients.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgClients.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgClients.EnableHeadersVisualStyles = false;
+            this.dgClients.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.dgClients.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dgClients.Location = new System.Drawing.Point(23, 65);
+            this.dgClients.Name = "dgClients";
+            this.dgClients.ReadOnly = true;
+            this.dgClients.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgClients.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgClients.RowHeadersWidth = 102;
+            this.dgClients.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dgClients.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgClients.Size = new System.Drawing.Size(1219, 957);
+            this.dgClients.TabIndex = 11;
+            // 
+            // tileCreate
+            // 
+            this.tileCreate.ActiveControl = null;
+            this.tileCreate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tileCreate.Location = new System.Drawing.Point(1122, 15);
+            this.tileCreate.Name = "tileCreate";
+            this.tileCreate.Size = new System.Drawing.Size(105, 44);
+            this.tileCreate.Style = MetroFramework.MetroColorStyle.Green;
+            this.tileCreate.TabIndex = 10;
+            this.tileCreate.Text = "Create";
+            this.tileCreate.UseSelectable = true;
+            this.tileCreate.UseStyleColors = true;
+            this.tileCreate.UseTileImage = true;
+            this.tileCreate.Click += new System.EventHandler(this.tileCreate_Click);
+            // 
+            // ClientsMainControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dgClients);
+            this.Controls.Add(this.tileCreate);
+            this.Font = new System.Drawing.Font("Bahnschrift", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "ClientsMainControl";
+            this.Padding = new System.Windows.Forms.Padding(23, 65, 23, 22);
+            this.Size = new System.Drawing.Size(1265, 1044);
+            ((System.ComponentModel.ISupportInitialize)(this.dgClients)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroGrid dgClients;
+        private MetroFramework.Controls.MetroTile tileCreate;
+    }
+}
